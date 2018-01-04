51f77260d1ba9fe48ea194bd279f7388b00b877e
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-04 14:38:53 +0800

    Spinner的應用

--- app/src/main/java/tw/com/shiaoshia/ex2018010403/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010403/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -13,12 +15,18 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     ArrayList<String> data;
+    TextView tv;
+    EditText et;
+    TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv = (TextView)findViewById(R.id.textView);
+        et = (EditText)findViewById(R.id.editText);
+        tv2 = (TextView)findViewById(R.id.textView3);
         spinner = (Spinner)findViewById(R.id.spinner);
         data = new ArrayList<>();
         data.add("AA11");
@@ -28,14 +36,26 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_expandable_list_item_1,data);
         spinner.setAdapter(adapter);
-        /*
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,data.get(i),Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                tv.setText(data.get(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
-        */
 
+    }
+    public void click01(View v) {
+        data.add(et.getText().toString());
+    }
+
+    public void click02(View v) {
+
+        int pos = spinner.getSelectedItemPosition();
+        tv2.setText(data.get(pos));
     }
 }
