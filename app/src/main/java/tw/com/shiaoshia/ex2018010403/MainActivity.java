48e28a4f33f0affa6fecbeaba5da85e7559b81b7
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-04 15:36:55 +0800

    getResources().getStringArray(R.array.cities); //從Resources取出資料

--- app/src/main/java/tw/com/shiaoshia/ex2018010403/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010403/MainActivity.java
@@ -13,11 +13,11 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner spinner;
+    Spinner spinner,spinner2;
     ArrayList<String> data;
     TextView tv;
     EditText et;
-    TextView tv2;
+    TextView tv2,tv4;
     ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView)findViewById(R.id.textView);
         et = (EditText)findViewById(R.id.editText);
         tv2 = (TextView)findViewById(R.id.textView3);
+        tv4 = (TextView)findViewById(R.id.textView4);
         spinner = (Spinner)findViewById(R.id.spinner);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+
         data = new ArrayList<>();
         data.add("AA11");
         data.add("BB22");
@@ -40,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 tv.setText(data.get(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String[] str = getResources().getStringArray(R.array.cities); //從Resources取出資料
+                tv4.setText(str[i]);
             }
 
             @Override
